786221b444b091b0a19f27b696d883a11962109d
Nathan Ellenfieldm <nathan.ellenfield@gmail.com>
2023-03-05 12:19:28 -0500

    Test out building using Fake

---
+++ PriceComparison/build/build.fs
@@ -0,0 +1,22 @@
+﻿open Fake.Core
+open Fake.DotNet
+
+open Fake.Core.TargetOperators
+
+let DefineTargets () = 
+  Target.create "Init" (fun _ -> Trace.log "---Initializing Build---" )
+  Target.create "Build" (fun _ -> DotNet.build id "")
+
+  "Init"
+    ==> "Build"
+
+[<EntryPoint>]
+let main args = 
+  args
+  |> Array.toList
+  |> Context.FakeExecutionContext.Create false "build.fsx"
+  |> Context.RuntimeContext.Fake 
+  |> Context.setExecutionContext
+  DefineTargets ()
+  Target.runOrDefaultWithArguments "Build"
+  0
